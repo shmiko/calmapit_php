cc0d5424b7cad511efadeb40ec5ef886498d4d3a
shmiko <shmiko@gmail.com>
2016-08-13 23:33:28 +1000

    src files

---
+++ Mapping/calmapito.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  var map;
+
+  function initMap(){
+  	//constructor creates a new map - only center and zoom are required
+  	map = new google.maps.Map(document.getElementById('map'),{
+  		center: {lat: 40.7413549, lng: -73.9980244},
+  		zoom: 13
+  	});
+  }
+
+
+}())
