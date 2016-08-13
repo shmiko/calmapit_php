7e5751ca57f91a40f6b4a227c7e82239cfa9ccdc
shmiko <shmiko@gmail.com>
2016-08-14 00:06:34 +1000

    api

--- Mapping/calmapito.js
+++ Mapping/calmapito.js
@@ -1,17 +1,19 @@
-(function() {
+// (function() {
   'use strict';
 
-  var map;
   var browserAPIKey = "AIzaSyBvLNxZkZ9Nn69-QSghPArPGgvHcrYDoS0";
   var clientID = "1077954538494-n7adn1hemab92p21e2h86rusg9vhhtb7.apps.googleusercontent.com";
 
-  function initMap(){
-  	//constructor creates a new map - only center and zoom are required
-  	map = new google.maps.Map(document.getElementById('map'),{
-  		center: {lat: 40.7413549, lng: -73.9980244},
-  		zoom: 13
-  	});
-  }
 
 
-}())
+	var map;
+	function initMap() {
+		// Constructor creates a new map - only center and zoom are required.
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 40.7413549, lng: -73.9980244},
+			zoom: 13
+		});
+	}
+
+
+// }())
