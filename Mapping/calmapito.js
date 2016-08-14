fd6bff901d893fe9cc16960ed988b5f7dddfcdb0
shmiko <shmiko@gmail.com>
2016-08-14 18:28:07 +1000

    info window

--- Mapping/calmapito.js
+++ Mapping/calmapito.js
@@ -7,12 +7,29 @@
 
 
 	var map;
+
 	function initMap() {
 		// Constructor creates a new map - only center and zoom are required.
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: -33.869831, lng: 151.196872 },
-			zoom: 8
+			zoom: 12
 		});
+
+        var opera_house = {lat: -33.856159, lng: 151.215256};
+        var marker = new google.maps.Marker({
+          position: opera_house,
+          map: map,
+          title: 'Sydney Opera House'
+        });
+        
+        var infowindow = new google.maps.InfoWindow({
+        	content: 'The Sydney Opera House is a major tourist attraction on the harbour' + 
+        	' with direct view of the Sydney Harbour Bridge'
+        });
+        marker.addListener('click', function(){
+        	//arguments are for the destination map and the anchor point - if you do not use the marker variable you would specify a point.
+    		infowindow.open(map, marker);
+        });
 	}
 
 
