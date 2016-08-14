ac5b16c1c9d65844c49f118986ba5138adc9e92b
shmiko <shmiko@gmail.com>
2016-08-14 10:29:31 +1000

    coords to sydney

--- Mapping/calmapito.js
+++ Mapping/calmapito.js
@@ -10,8 +10,8 @@
 	function initMap() {
 		// Constructor creates a new map - only center and zoom are required.
 		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: 40.7413549, lng: -73.9980244},
-			zoom: 13
+			center: {lat: -33.869831, lng: 151.196872 },
+			zoom: 8
 		});
 	}
 
