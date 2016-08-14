0a2fe0940f2b6b3cc95ebf13a09643d7a876b6a0
shmiko <shmiko@gmail.com>
2016-08-14 20:37:31 +1000

    multiple markers show hide

--- Mapping/calmapito.js
+++ Mapping/calmapito.js
@@ -1,21 +1,19 @@
 // (function() {
   'use strict';
 
-  var MYAPIKEY = "AIzaSyBvLNxZkZ9Nn69-QSghPArPGgvHcrYDoS0";
-  var clientID = "1077954538494-n7adn1hemab92p21e2h86rusg9vhhtb7.apps.googleusercontent.com";
-
-
+	var MYAPIKEY = "AIzaSyBvLNxZkZ9Nn69-QSghPArPGgvHcrYDoS0";
+	var clientID = "1077954538494-n7adn1hemab92p21e2h86rusg9vhhtb7.apps.googleusercontent.com";
 
 	var map;
 	var markers = [];
 
 	function initMap() {
-		// Constructor creates a new map - only center and zoom are required.
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: -33.869831, lng: 151.196872 },
-			zoom: 14
-		});
-
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.856159, lng: 151.215256},
+          zoom: 13,
+          mapTypeControl: false
+        });
         // var opera_house = {lat: -33.856159, lng: 151.215256};
         // var oldMarker = new google.maps.Marker({
         //   position: opera_house,
@@ -34,15 +32,13 @@
         ];
 
         var largeInfowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < locations.length; i++) {
           // Get the position from the location array.
           var position = locations[i].location;
           var title = locations[i].title;
           // Create a marker per location, and put into markers array.
-          var marker = new google.maps.Marker({
-            map: map,
+           var marker = new google.maps.Marker({
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
@@ -54,35 +50,40 @@
           marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
           });
-          bounds.extend(markers[i].position);
         }
-        // Extend the boundaries of the map for each marker
-        map.fitBounds(bounds);
-
-        
-
-        // var infowindow = new google.maps.InfoWindow({
-        // 	content: 'The Sydney Opera House is a major tourist attraction on the harbour' + 
-        // 	' with direct view of the Sydney Harbour Bridge'
-        // });
-      	//   marker.addListener('click', function(){
-      	//   	//arguments are for the destination map and the anchor point - if you do not use the marker variable you would specify a point.
-    		// infowindow.open(map, marker);
-      	//   });
-	}
-
-	//This will populate the info window when the marker is clicked.
-    function populateInfoWindow(marker, infowindow) {
+        document.getElementById('show-listings').addEventListener('click', showListings);
+        document.getElementById('hide-listings').addEventListener('click', hideListings);
+      }
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
           infowindow.setContent('<div>' + marker.title + '</div>');
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker(null);
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
           });
         }
-    }
+      }
+      // This function will loop through the markers array and display them all.
+      function showListings() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+      // This function will loop through the listings and hide them all.
+      function hideListings() {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      }
 
 // }())
